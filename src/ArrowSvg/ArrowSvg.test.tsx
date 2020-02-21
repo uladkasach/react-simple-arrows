db4eb103af26bbf3aca87d93bfae0675c06552e2
Jim Renwick <jim.renwick@webgp.com>
2020-02-21 16:53:41 +0000

    rendering on background colours was showing HighlightArrowHeadSvg, so have removed and replaced with a alternate method to hide the end of highlighted line

--- src/ArrowSvg/ArrowSvg.test.tsx
+++ src/ArrowSvg/ArrowSvg.test.tsx
@@ -5,10 +5,7 @@ import { render } from '@testing-library/react';
 import { LineOrientation } from '../constants';
 import { ArrowSvg } from './ArrowSvg';
 
-jest.mock('uuid', () => {
-  let counter = 1;
-  return () => ('__UUID' + counter++)
-} );
+jest.mock('uuid', () => () => '__UUID__');
 
 describe('ArrowSvg', () => {
 
@@ -23,12 +20,18 @@ describe('ArrowSvg', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
   it('should look correct going down and right with highlight default color enabled', async () => {
-    const { container } = render(<ArrowSvg start={{ x: 0, y: 0 }} end={{ x: 500, y: 300 }} orientation={LineOrientation.HORIZONTAL} highlight={true}  />);
+    const { container } = render(
+        <body style={ { backgroundColor: 'blue' } }>
+        <ArrowSvg start={{ x: 0, y: 0 }} end={{ x: 500, y: 300 }} orientation={LineOrientation.HORIZONTAL} highlight={true}  />
+        </body>);
     expect(container.firstChild).not.toEqual(null);
     expect(container.firstChild).toMatchSnapshot();
   });
   it('should look correct going down and right with highlight custom color enabled', async () => {
-    const { container } = render(<ArrowSvg start={{ x: 0, y: 0 }} end={{ x: 500, y: 300 }} orientation={LineOrientation.HORIZONTAL} highlight={true} highlightColor="burlywood"/>);
+    const { container } = render(
+        <body style={ { backgroundColor: 'blue' } }>
+        <ArrowSvg start={{ x: 0, y: 0 }} end={{ x: 500, y: 300 }} orientation={LineOrientation.HORIZONTAL} highlight={true} highlightColor="burlywood"/>
+        </body>);
     expect(container.firstChild).not.toEqual(null);
     expect(container.firstChild).toMatchSnapshot();
   });
